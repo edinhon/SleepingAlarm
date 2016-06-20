671501c3d2ef91d3d26fbbfbd4fface70a657a91
edinhon <m23127654@gmail.com>
2016-06-20 18:02:10 +0800

    Play default music while alarm ringing.

--- app/src/main/java/com/example/nthucs/sleepingalarm/RingAlarmDialogActivity.java
+++ app/src/main/java/com/example/nthucs/sleepingalarm/RingAlarmDialogActivity.java
@@ -3,6 +3,7 @@ package com.example.nthucs.sleepingalarm;
 import android.app.Activity;
 import android.app.Service;
 import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.os.Vibrator;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,8 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
+
+import java.io.IOException;
 
 public class RingAlarmDialogActivity extends Activity {
 
@@ -23,13 +26,29 @@ public class RingAlarmDialogActivity extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
 
+        //Show Toast.
         Toast.makeText(this, "Ring", Toast.LENGTH_LONG).show();
 
+        //Let device vibrate.
         final Vibrator vibrator = (Vibrator)getSystemService(Service.VIBRATOR_SERVICE);
         if(vibrator.hasVibrator()){
             vibrator.vibrate(new long[]{1000, 1000}, 0);
         }
 
+        //Play music.
+        //MediaPlayer mp = new MediaPlayer();
+        /*try {
+            mp.setDataSource("");
+            mp.start();
+            mp.setLooping(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.shot);
+        mp.start();
+
+
+        //Show dialog.
         final View item = LayoutInflater.from(this).inflate(R.layout.alert_dialog_ring_alarm, null);
         new AlertDialog.Builder(this)
                 .setTitle("Alarm Ring")
@@ -38,6 +57,7 @@ public class RingAlarmDialogActivity extends Activity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         vibrator.cancel();
+                        mp.stop();
                         finish();
                     }
                 })
